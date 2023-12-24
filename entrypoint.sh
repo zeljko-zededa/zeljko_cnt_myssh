b3df3765715601929ee1f3d3943ce8ae33aba1e1
Zeljko Misic <zeljko@zededa.com>
2023-12-24 17:48:07 +0100

    again out  some tees to the printfs in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 varoutputfile=$WORKDIR/zededa_environment_variables.txt
-printf 'ENV1 is: %s\n' "${1}" | tee zededa_environment_variables.txt
-printf 'ENV2 is: %s\n' "${2}" | tee >> zededa_environment_variables.txt
-printf 'ENV3 is: %s\n' "${2}" | tee >> zededa_environment_variables.txt
+printf 'ENV1 is: %s\n' "${1}" | tee $varoutputfile
+printf 'ENV2 is: %s\n' "${2}" | tee >> $varoutputfile
+printf 'ENV3 is: %s\n' "${2}" | tee >> $varoutputfile
 /usr/sbin/sshd -D
