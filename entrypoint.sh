876e2cfd0b25d76d6d6b55a19701eeda66f00cfe
Zeljko Misic <zeljko@zededa.com>
2023-12-24 14:32:21 +0100

    put some echos in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 my_outfile=zededa-environment-variables.txt
+echo "${1}"
+echo "${2}"
+echo "${3}"
 echo "${1}" >> $my_outfile
 echo "${2}" >> $my_outfile
 echo "${3}" >> $my_outfile
